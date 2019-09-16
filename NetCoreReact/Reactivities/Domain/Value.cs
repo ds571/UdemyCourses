0260147ac1c1b5d93fc52fd93788806a4d485d09
MS\dsandov3 <daniel.sandoval@optum.com>
2019-09-16 14:06:43 -0700

    Added changes made at office

---
+++ NetCoreReact/Reactivities/Domain/Value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        public int Id { get; set; } // EF automatically makes Id the Primary Key. As Integer, will also be configured as auto-generated number
+        public string Name{ get; set; }
+    }
+}
---
+++ NetCoreReact/Reactivities/client-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  // lifecycle method
+  componentDidMount() {
+    this.setState({
+      values: [{id: 1, name: 'Value 101'}, {id: 2, name: 'Value 102'}]
+    })
+  }
+
+  render() {
+
+    return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          {this.state.values.map((value: any) => (
+            <li>{value.name}</li>
+          ))}
+        </ul>
+      </header>
+    </div>);
+  }
+}
+
+export default App;
